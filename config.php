7438939b9318f0dd49f3cf0e5d9b19400cf69e1b
Tim Lytle <tim@timlytle.net>
2013-01-10 12:17:20 -0500

    Added GitHub Page

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-//load defaults from config file, allow enviroment override
-$config = parse_ini_file(__DIR__ . '/config.ini');
-foreach($config as $setting => $default){
-    defined($setting)
-    || define($setting, (getenv($setting) ? getenv($setting) : $default));
-}
